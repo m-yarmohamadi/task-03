e3d85aaf67e821a0c1884d8717bdd2951b9ed7c7
m-yarmohamadi <yarmohamadi677@gmail.com>
2024-05-16 19:45:36 +0330

    first Commit

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
 	return (
 		<div className='xl:h-screen md:container flex flex-col justify-center items-center gap-y-20 py-32 mx-4'>
 			<div className='w-full flex justify-center md:justify-start items-center  mb-2'>
-				<h1 className=' font-extrabold text-2xl md:text-3xl text-secondary-800'>سبد دوره های ششما</h1>
+				<h1 className=' font-extrabold text-2xl md:text-3xl text-secondary-800'>سبد دوره های شما</h1>
 			</div>
 			<div className='w-full grid grid-cols-1 lg:grid-cols-3  gap-y-24 lg:gap-x-8'>
 				<div className=' flex flex-col justify-between items-center col-span-2 gap-y-6 w-full'>
